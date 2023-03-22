92979a47a2554aaff97aa50bd12cd539c691778a
Topthinking <nio@MacBook-Pro-2.local>
2023-03-22 18:08:53 +0800

    Site updated: 2023-03-22 18:08:53

--- cocos2d/plane-fight/index.js
+++ cocos2d/plane-fight/index.js
@@ -8,18 +8,23 @@ System.register(["./application.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationJs) {
-      Application = _applicationJs.Application;
-    }],
+    setters: [
+      function (_applicationJs) {
+        Application = _applicationJs.Application;
+      },
+    ],
     execute: function () {
       application = new Application();
-      topLevelImport('cc').then(function (engine) {
-        return application.init(engine);
-      }).then(function () {
-        return application.start();
-      })["catch"](function (err) {
-        console.error(err);
-      });
-    }
+      topLevelImport("cc")
+        .then(function (engine) {
+          return application.init(engine);
+        })
+        .then(function () {
+          return application.start();
+        })
+        ["catch"](function (err) {
+          console.error(err);
+        });
+    },
   };
 });
